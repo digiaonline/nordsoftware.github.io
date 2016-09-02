00b7f121c845155954490d28e91ebf28a76420b0
Chi Nguyen <bigfish12324@gmail.com>
2016-09-02 14:06:14 +0300

    fix typo favicon

--- web_modules/LayoutContainer/index.js
+++ web_modules/LayoutContainer/index.js
@@ -49,18 +49,18 @@ export default class Layout extends Component {
           },
           ] }
           link={ [
-            { "rel": "apple-touch-icon",
-              "href": "/apple-touch-icon.png", "sizes":"180x180",
+            { rel: "apple-touch-icon",
+              href: "/apple-touch-icon.png", sizes:"180x180",
             },
-            { "rel": "icon", type:"image/png",
-              "href": "/favicon-32x32.png", "sizes":"32x32",
+            { rel: "icon", type:"image/png",
+              href: "/favicon-32x32.png", sizes:"32x32",
             },
-            { "rel": "icon", type:"image/png",
-              "href": "/favicon-16x16.png", "sizes":"16x16",
+            { rel: "icon", type:"image/png",
+              href: "/favicon-16x16.png", sizes:"16x16",
             },
-            { "rel": "manifest", "href": "/manifest.json" } ,
-            { "rel": "mask-icon", "href": "/safari-pinned-tab.svg",
-              "color": "#5bbad5",
+            { rel: "manifest", href: "/manifest.json" } ,
+            { rel: "mask-icon", href: "/safari-pinned-tab.svg",
+              color: "#5bbad5",
             },
           ] }
         />
