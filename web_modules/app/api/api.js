c4a8d88e6eaefd5b09f2affb580a1bc0dc5410f4
Chi Nguyen <chi.nguyen@nordsoftware.com>
2016-10-28 14:37:53 +0300

    add new token

--- web_modules/app/api/api.js
+++ web_modules/app/api/api.js
@@ -1,7 +1,7 @@
 export function fetchRepoData(page) {
   return fetch(
     "https://api.github.com/orgs/nordsoftware/repos" + "?page=" + page +
-    "&access_token=" + "136068275cf127f2e8dee5ec7a7f18c7dda14912"
+    "&access_token=" + "2199dcb3e3523d9c8a31c6c5567c88eb34840cc5"
   )
     .then((response) => response.json())
     .then((data) => {
